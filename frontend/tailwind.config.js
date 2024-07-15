dac173e7f10cee8ea6ce702468748768e5c004df
Abdelrhman Khalifa <112504016+AbduKhalifa@users.noreply.github.com>
2024-07-15 11:40:50 +0300

    initial front end
    add vars to index.css & tailwindcssConfig

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'base':'#121618',
+        'helper':'#FFC851',
+        'text':'#F7FFFF',
+        'dark':'#686A6B'
+      },
+      fontSize:{
+        'ex-small':"13px",
+        'small':'14px',
+        'medium':'16px',
+        'big':'18px',
+        'xbig':'32px',
+      },
+      transitionDuration:{
+        "fast":"300ms",
+        "medium":"450ms",
+        "slow":"850ms",
+        "second":"1s"
+      }
+    },
   },
   plugins: [],
 }
